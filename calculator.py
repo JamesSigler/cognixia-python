e263b0b12c0a3cb9ac945c922a7fd22291ab9ada
JamesSigler <james64.sigler@ttu.edu>
2021-02-24 11:33:26 -0700

    Test

--- calculator.py
+++ calculator.py
@@ -26,19 +26,22 @@ while check:
     print("5. Power")
     print("6. Exit")
 
+    choice = input("Enter a choice: ")
+
     num1, num2 = input("Enter two numbers seperated by a comma:").split(",")
     num1, num2 = int(num1), int(num2)
     
     if choice == 1:
-        print(add(num1,xnum2))
+        print(add(num1,num2))
     elif choice == 2:
-        print(sub(num1,xnum2))
+        print(sub(num1,num2))
     elif choice == 3:
-        print(mult(num1,xnum2))
+        print(mult(num1,num2))
     elif choice == 5:
-        print(div(num1,xnum2))
+        print(div(num1,num2))
     elif choice == 4:
-        print(pow(num1,xnum2))
-    elif choice == 6
+        print(pow(num1,num2))
+    elif choice == 6:
+        check = True
     
         
