66b0b378c0b9b549de7fbc95f0bcf794b7a15843
mariatan0 <mariaetdc@gmail.com>
2021-02-24 11:29:37 -0600

    initial commit for calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    print("Sum: ", x + y)
+
+def sub(x,y):
+    print("Subtraction: ", x - y)
+
+def mult(x,y):
+    print("Multiply: ", x * y)
+
+def div(x,y):
+    print("Divide: ", x / y)
+
+def pow(x,y):
+    print("Power: ", x^y)
+
+
